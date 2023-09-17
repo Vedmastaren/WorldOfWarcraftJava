f675d7e97b1a252faa64c15e6c434f46dc15082e
Mellblomman <115022366+Mellblomman@users.noreply.github.com>
2023-09-17 21:43:59 +0200

    ")" i priest & rogue

--- src/Priest.java
+++ src/Priest.java
@@ -2,8 +2,8 @@ public class Priest {
     int hp = 100;
     int mana = 200;
 
-    double autoattack = (Math.floor(Math.random() * 5 + 5);
-    double  flashHeal = (Math.floor(Math.random() * 20 + 20);
-    double  greaterHeal = (Math.floor(Math.random() * 30 + 30);
+    double autoattack = (Math.floor(Math.random() * 5 + 5));
+    double  flashHeal = (Math.floor(Math.random() * 20 + 20));
+    double  greaterHeal = (Math.floor(Math.random() * 30 + 30));
 
 }
