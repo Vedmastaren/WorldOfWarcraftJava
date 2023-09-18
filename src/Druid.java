05ade1bde8a1f12baac9fd38684332d755a9ee9f
Vedmastaren <117941745+Vedmastaren@users.noreply.github.com>
2023-09-18 12:21:48 +0200

    New comment "next"

--- src/Druid.java
+++ src/Druid.java
@@ -1,8 +1,21 @@
 public class Druid {
+    Scanner scan = new Scanner(System.in);
 
     double autoattack = (Math.floor(Math.random() * 5 + 5);
-    boolean catForm = false;
-    boolean bearForm = false;
-    boolean travelForm = false;
+    boolean catForm = 0;
+    boolean bearForm = 0;
+    boolean travelForm = 0;
+    System.out.println("Enter a number to change form (1 = bear-, 2 = cat-, 3 = travelform: ")
+    if(boolean == 1) {
+        boolean catForm = true;
+        else if (boolean bearForm == 2) {
+            boolean bearForm = true;
+            else if (boolean travelForm == 3) {
+                boolean travelForm = true;
+            }
+        }
     }
+
+    }
+
 
