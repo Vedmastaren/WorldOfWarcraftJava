41479ad1d3c7766b302a5dc134a24909bfce0485
Mellblomman <115022366+Mellblomman@users.noreply.github.com>
2023-09-18 12:35:40 +0200

    Edited Druid

--- src/Druid.java
+++ src/Druid.java
@@ -1,22 +1,33 @@
-public class Druid {
-    Scanner scan = new Scanner(System.in);
+import java.util.Scanner;
+public class Druid
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
 
-    double autoattack = (Math.floor(Math.random() * 5 + 5);
-    boolean catForm = 0;
-    boolean bearForm = 0;
-    boolean travelForm = 0;
-    System.out.println("Enter a number to change form (1 = bear-, 2 = cat-, 3 = travelform: ")
-            int choice = scan.nextInt();)
-    if(choice == 1) {
-        boolean catForm = true;
-        else if (choice bearForm == 2) {
-            boolean bearForm = true;
-            else if (choice travelForm == 3) {
-                boolean travelForm = true;
-            }
+
+        double autoattack = (Math.floor(Math.random() * 5 + 5));
+        boolean catForm = false;
+        boolean bearForm = false;
+        boolean travelForm = false;
+
+        System.out.println("Enter a number to change form (1 = bear-, 2 = cat-, 3 = travelform: ");
+        int choice = scan.nextInt();
+        if(choice == 1)
+        {
+            catForm = true;
+
+        }
+        else if (choice == 2)
+        {
+            bearForm = true;
+        }
+        else if (choice == 3)
+        {
+            travelForm = true;
         }
     }
+}
 
-    }
 
 
