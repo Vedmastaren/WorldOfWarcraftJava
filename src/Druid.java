1d884dafcf441a6d1070aba13198f812d5d54626
Vedmastaren <117941745+Vedmastaren@users.noreply.github.com>
2023-09-18 12:18:26 +0200

    New comment "next"

--- src/Druid.java
+++ src/Druid.java
@@ -1,6 +1,8 @@
 public class Druid {
 
     double autoattack = (Math.floor(Math.random() * 5 + 5);
-    
+    boolean catForm = false;
+    boolean bearForm = false;
+    boolean travelForm = false;
     }
 
